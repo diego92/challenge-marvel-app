ac7d1c51b0664463726bccf843c7c6f68e9fca6f
Diego Gonzalez <diego_bosteroxeneize@hotmail.com>
2021-11-24 07:32:18 -0300

    Se arma estructura de proyecto

---
+++ src/pages/hero/HeroDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HeroDetail = () => {
+    return (
+        <div>
+            Detail
+        </div>
+    )
+}
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,37 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/heroes" />
+          <Route exact path="/heroes/:heroId" />
+          <Redirect to="heroes" />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
